1461db795e97c686c3f1cb8561921658ba781eae
Alex <midnava@gmal.com>
2025-12-15 11:57:53 +0100

    senior interview tasks

--- interview/src/task5/GCFreeMethod.java
+++ interview/src/task5/GCFreeMethod.java
@@ -1,11 +1,19 @@
 package task5;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Refactor this method to be GC-free.
  * <p>
  * Duration 10 minutes.
  */
 public class GCFreeMethod {
+    private final Map<Integer, Double> map = new HashMap<>();
+
+    public void put(int id, double price) {
+        map.put(id, price); //any performance issue?
+    }
 
     public void doSomethingWithList() {
         StringBuilder builder = new StringBuilder(); // avoid allocating on every call
